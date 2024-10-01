5d7d9e05d0f2dff5c43b74194ae55adc7fc2e7cc
ferbarcante <barcantef@gmail.com>
2024-10-01 11:13:26 -0300

    get e set

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
 
 class Pessoa{
-    public $nome;
-    public $cpf;
-    public $rg;
-    public $email;
-    public $endereco;
-    public $cidade;
-    public $uf;
-    public $cep;
+    private $nome;
+    private $cpf;
+    private $rg;
+    private $email;
+    private $endereco;
+    private $cidade;
+    private $uf;
+    private $cep;
 
     public function __construct($nome, $cpf, $rg, $email){
         $this->nome = $nome;
@@ -17,10 +17,50 @@ class Pessoa{
         $this->email = $email;
     }
 
+    public function welcome(){
+        echo "Seja bem vindo(a) ", $this->nome;
+    }
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setNome($nome){
+        $this->nome = $nome;
+    }
     
+    public function getCpf(){
+        return $this->cpf;
+    }
+
+    public function setCpf($cpf){
+        $this->cpf = $cpf;
+    }
+
+    public function getEnder(){
+        return $this->endereco;
+    }
+
+    public function setEnder($endereco){
+        $this->endereco = $endereco;
+    }
+
+    public function getCidade(){
+        return $this->cidade;
+    }
+
+    public function setCidade($cidade){
+        $this->cidade = $cidade;
+    }
 }
 
 $rodrigo = new Pessoa("Rodrigo", "98892839489", "9382948923", "rodrigo@gmail.com");
-echo "nome: ", $rodrigo->nome, " cpf: ", $rodrigo->cpf;
+echo "nome: ", $rodrigo->getNome(), " cpf: ", $rodrigo->getCpf();
+echo"<br>";
 
+$rodrigo->setCidade("Salvador");
+echo"<br>", "cidade: ", $rodrigo->getCidade();
+
+echo"<br>";
+$rodrigo->welcome();
 ?>
