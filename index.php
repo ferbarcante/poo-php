5991649180081ef16d34942754956759e7e989a3
ferbarcante <barcantef@gmail.com>
2024-10-19 10:12:38 -0300

    assosiative arrays

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
 
-require 'classes/produto.php';
-require 'models/produto.php';
+        $books = [
+            [
+                'name' => "Irmãos Karamazov",
+                'author' => "Fiodor Dostoievsky",
+                'price' => 70.00
+            ],
+            [
+                'name' => 'Meditaçoes',
+                'author' => 'Marco Aurelio',
+                'price' => 20
+            ],
+            [
+                'name' => "Noites Brancas",
+                'author' => "Fiodor Dostoievsky",
+                'price' => 60
+            ]
 
-$produto = new \models\Produto();
-$produto->mostrarDetalhes;
+        ];
+
+
+    ?>
+
+    <ul>
+        <?php foreach ($books as $book) : ?>
+            <?php if($book['author']  == 'Fiodor Dostoievsky') : ?>
+                 <li><?= $book['name']; ?></li>
+            <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
+
